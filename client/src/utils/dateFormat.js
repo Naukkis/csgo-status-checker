460d690192026c4b4c47dbfc28be6b52fd03a9db
Naukkis <22678066+Naukkis@users.noreply.github.com>
2018-04-28 16:38:15 +0300

    switch dayssince from round() -> ceil()

--- client/src/utils/dateFormat.js
+++ client/src/utils/dateFormat.js
@@ -1,3 +1,3 @@
 export const dateFormat = addedAt => new Date(addedAt).toLocaleDateString();
 
-export const countDaysSinceToday = addedAt => Math.round((new Date() - new Date(addedAt)) / (1000 * 60 * 60 * 24));
+export const countDaysSinceToday = addedAt => Math.ceil((new Date() - new Date(addedAt)) / (1000 * 60 * 60 * 24));
