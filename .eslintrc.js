658d55aa188b6e766293dc23e07be782b0f06905
Naukkis <22678066+Naukkis@users.noreply.github.com>
2018-04-29 22:51:48 +0300

    loosen eslint more

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,10 @@ module.exports = {
         "linebreak-style": 0,
         "no-console": 0,
         "no-use-before-define": ["error", { "functions": false, "classes": true }],
-        "react/forbid-prop-types": 0
+        "react/forbid-prop-types": 0,
+        "prefer-destructuring": ["error", {
+          "array": false,
+          "object": true
+        }]
     }
 };
